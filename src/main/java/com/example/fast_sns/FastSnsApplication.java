170842911279c20ab87458bc5d9ba46a22058763
JaeSuk <110963294+suhjaesuk@users.noreply.github.com>
2023-07-07 15:36:21 +0900

    git init

---
+++ src/main/java/com/example/fast_sns/FastSnsApplication.java
@@ -0,0 +1,13 @@
+package com.example.fast_sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fast_sns/FastSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fast_sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
